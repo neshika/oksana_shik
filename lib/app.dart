425a1b2145efd3cd88c44472ff6540c96e119768
neshika <stepanovaml@mail.ru>
2025-08-20 15:48:01 +0300

    structure

---
+++ lib/app.dart
@@ -0,0 +1,30 @@
+// lib/app.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+import 'routes/app_router.dart';
+import 'state/store.dart';
+import 'utils/theme.dart';
+import 'utils/localization.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreProvider(
+      store: AppStore.instance.store,
+      child: MaterialApp(
+        title: 'Оксана Шик',
+        theme: AppTheme.light,
+        darkTheme: AppTheme.dark,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: const Locale('ru'),
+        onGenerateRoute: AppRouter.generateRoute,
+        initialRoute: '/splash',
+      ),
+    );
+  }
+}
